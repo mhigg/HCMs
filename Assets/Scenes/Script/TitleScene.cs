9ce331b861eaa1b43a8e39ede261f52865393d91
Natuki_GoE <natu0617k@gmail.com>
2020-05-13 04:10:26 +0900

    Never Trembling Race の基盤 シーン移行の実装(TitleからMenuのみ) シーンの追加

---
+++ Assets/Scenes/Script/TitleScene.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    bool isCalledOnce = false;
+
+    void Update()
+    {
+        if (!isCalledOnce)
+        {
+
+            if (Input.GetKeyDown("space"))
+            {
+                isCalledOnce = true;
+                FadeManager.Instance.LoadScene("MenuScene", 2.0f);
+                Debug.Log("Menuへ");
+            }
+        }
+    }
+}
