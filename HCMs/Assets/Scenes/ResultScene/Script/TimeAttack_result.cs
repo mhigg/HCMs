0b37a69ba551331e6a174dbd1bf84a8f8e3a734b
DaichiNakagama <nakadaichi@icloud.com>
2020-05-13 12:14:47 +0900

    シーン移行のすくりぷと

---
+++ HCMs/Assets/Scenes/ResultScene/Script/TimeAttack_result.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeAttack_result : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    bool isCalledOnce = false;
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isCalledOnce)
+        {
+            ///ここを任意のボタンにしましょう。
+            if (Input.GetKeyDown("space"))
+            {
+                //タイトルに戻る
+                isCalledOnce = true;
+                FadeManager.Instance.LoadScene("TitleScene", 2.0f);
+                Debug.Log("Titleへ");
+                //もしくはメニューに戻る
+            }
+        }
+    }
+}
