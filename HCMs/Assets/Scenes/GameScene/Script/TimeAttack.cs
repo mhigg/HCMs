e40b0d7ea23f84d63dfaaaf026c47c0505a3d8b5
DaichiNakagama <nakadaichi@icloud.com>
2020-05-13 12:17:21 +0900

    しーんいこうのすくりぷと ふぉるだのついか

---
+++ HCMs/Assets/Scenes/GameScene/Script/TimeAttack.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeAttack : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    bool isCalledOnce = false;
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isCalledOnce)
+        {
+            ///ここを任意のボタンにしましょう。
+            if (Input.GetKeyDown("space"))
+            {
+                isCalledOnce = true;
+                FadeManager.Instance.LoadScene("TimeAttackResult", 2.0f);
+                Debug.Log("Resultへ");
+            }
+        }
+    }
+}
