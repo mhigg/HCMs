02b2d5d4a49c7224265d475a6fee0c2ca1bbce59
Natuki_GoE <natu0617k@gmail.com>
2020-05-18 13:18:39 +0900

    BattleSceneの更新

--- HCMs/Assets/Control/CarControl.cs
+++ HCMs/Assets/Control/CarControl.cs
@@ -9,6 +9,8 @@ public class AxleInfo
     public WheelCollider rightWheel;
     public bool motor;    // このホイールがエンジンにアタッチされているかどうか
     public bool steering; // このホイールがハンドルの角度を反映しているかどうか
+    //public bool steering_Back;    // このホイールがエンジンにアタッチされているかどうか
+    //public bool motor_Back; // このホイールがハンドルの角度を反映しているかどうか
 }
 
 public class CarControl : MonoBehaviour
@@ -17,6 +19,9 @@ public class CarControl : MonoBehaviour
     public List<AxleInfo> axleInfos;    // 個々の車軸の情報
     public float maxMotorTorque;        // ホイールに適用可能な最大トルク
     public float maxSteeringAngle;      // 適用可能な最大ハンドル角度
+
+    //public float Back_maxMotorTorque;        // ホイールに適用可能な最大トルク
+    //public float Back_maxSteeringAngle;      // 適用可能な最大ハンドル角度
 
     public WheelCollider wheelFL;
     public WheelCollider wheelFR;
@@ -29,6 +34,8 @@ public class CarControl : MonoBehaviour
     public Transform wheelBRTrans;
     float steering = 0.0f;
     float motor = 0.0f;
+    //float steering_Back = 0.0f;
+    //float motor_Back = 0.0f;
 
     void Start()
     {
@@ -46,7 +53,8 @@ public class CarControl : MonoBehaviour
         // wheelcolliderの角度に合わせてタイヤモデルを回転する（フロントのみ）
         wheelFLTrans.localEulerAngles = new Vector3(wheelFLTrans.localEulerAngles.x, wheelFL.steerAngle - wheelFLTrans.localEulerAngles.z, wheelFLTrans.localEulerAngles.z);
         wheelFRTrans.localEulerAngles = new Vector3(wheelFRTrans.localEulerAngles.x, wheelFR.steerAngle - wheelFRTrans.localEulerAngles.z, wheelFRTrans.localEulerAngles.z);
-
+        wheelBLTrans.localEulerAngles = new Vector3(wheelBLTrans.localEulerAngles.x, wheelBL.steerAngle - wheelBLTrans.localEulerAngles.z, wheelBLTrans.localEulerAngles.z);
+        wheelBRTrans.localEulerAngles = new Vector3(wheelBRTrans.localEulerAngles.x, wheelBR.steerAngle - wheelBRTrans.localEulerAngles.z, wheelBRTrans.localEulerAngles.z);
 
     }
 
@@ -56,17 +64,26 @@ public class CarControl : MonoBehaviour
         motor = -maxMotorTorque * Input.GetAxis("Vertical");
         steering = maxSteeringAngle * Input.GetAxis("Horizontal");
 
+        //steering_Back = -Back_maxMotorTorque * Input.GetAxis("Vertical");
+        //motor_Back = Back_maxSteeringAngle * Input.GetAxis("Horizontal");
+
         foreach (AxleInfo axleInfo in axleInfos)
         {
             if (axleInfo.steering)
             {
                 axleInfo.leftWheel.steerAngle = steering;
                 axleInfo.rightWheel.steerAngle = steering;
+
+                //axleInfo.leftWheel.steerAngle = steering_Back;
+                //axleInfo.rightWheel.steerAngle = steering_Back;
             }
             if (axleInfo.motor)
             {
                 axleInfo.leftWheel.motorTorque = motor;
                 axleInfo.rightWheel.motorTorque = motor;
+
+                //axleInfo.leftWheel.motorTorque = motor_Back;
+                //axleInfo.rightWheel.motorTorque = motor_Back;
             }
         }
     }
