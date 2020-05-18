695123f18cd8e0c5daa3416e7e9bf4d2f9735797
mhigg <tekito@mail>
2020-05-18 11:01:08 +0900

    タイムの加算と表示確認

--- HCMs/Assets/TimeCount.cs
+++ HCMs/Assets/TimeCount.cs
@@ -1,18 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TimeCount : MonoBehaviour
 {
+    public Text timeText = null;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        timeText = timeText.GetComponent<Text>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // 毎ﾌﾚｰﾑごとにﾀｲﾑ加算
+        timeText.text = Time.time.ToString();
     }
 }
