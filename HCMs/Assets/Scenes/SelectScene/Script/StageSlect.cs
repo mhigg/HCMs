fd1e6a7edc973c58dd5182ed0f99b43962a3c24b
DaichiNakagama <nakadaichi@icloud.com>
2020-06-05 14:53:37 +0900

    タイムアタックの選択画面

---
+++ HCMs/Assets/Scenes/SelectScene/Script/StageSlect.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class StageSlect : MonoBehaviour
+{
+    GameObject _nowSelect;
+    public List<Image> stages;
+    [SerializeField] EventSystem eventSystem;
+    GameObject _selectObj;
+    List<string> _imageName;
+
+    void Start()
+    {
+        var tmp = stages.Count;
+        _imageName = new List<string>(tmp);
+        for (int i = 0; i < tmp; i++)
+        {
+            _imageName.Add($"{i}");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (eventSystem.currentSelectedGameObject.gameObject == _selectObj)
+        {
+        }
+        else
+        {
+            _selectObj = eventSystem.currentSelectedGameObject.gameObject;
+            for (int i = 0; i < stages.Count; i++)
+            {
+                if (_selectObj.name == _imageName[i])
+                {
+                    Vector3 pos = stages[i].transform.localPosition;
+                    pos.x = 0;
+                    stages[i].transform.localPosition = pos;
+                    pos.x = 1200;
+                    stages[(i + 1) % stages.Count].transform.localPosition = pos;
+                    pos.x = -1200;
+                    stages[(i + stages.Count - 1) % stages.Count].transform.localPosition = pos;
+                }
+            }
+            //if(_selectObj.name == "up")
+            //{
+            //    Vector3 pos = stages[0].transform.localPosition;
+            //    pos.x = 0;
+            //    stages[0].transform.localPosition = pos;
+            //    pos.x = 1200;
+            //    stages[1].transform.localPosition = pos;
+            //}
+            //if (_selectObj.name == "down")
+            //{
+            //    Vector3 pos = stages[1].transform.localPosition;
+            //    pos.x = 0;
+            //    stages[1].transform.localPosition = pos;
+            //    pos.x = 1200;
+            //    stages[0].transform.localPosition = pos;
+            //}
+
+        }
+    }
+}
