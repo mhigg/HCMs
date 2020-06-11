af6c7431d8b39bef840775db1db79d3d0f013135
DaichiNakagama <nakadaichi@icloud.com>
2020-06-11 15:12:37 +0900

    select スクリプト」

--- HCMs/Assets/Scenes/SelectScene/Script/BattleSel.cs
+++ HCMs/Assets/Scenes/SelectScene/Script/BattleSel.cs
@@ -1,26 +1,62 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class BattleSel : MonoBehaviour
 {
+    GameObject _nowSelect;
+    public List<Image> stages;
+    [SerializeField] EventSystem eventSystem;
+    GameObject _selectObj;
+    int _nowSelected = 0;
+    List<string> _imageName;
+
     void Start()
     {
-
+        var tmp = stages.Count;
+        _imageName = new List<string>(tmp);
+        for (int i = 0; i < tmp; i++)
+        {
+            _imageName.Add($"{i}");
+        }
     }
 
     bool isCalledOnce = false;
 
+    // Update is called once per frame
     void Update()
     {
+        if (eventSystem.currentSelectedGameObject.gameObject == _selectObj)
+        {
+        }
+        else
+        {
+            _selectObj = eventSystem.currentSelectedGameObject.gameObject;
+            for (int i = 0; i < stages.Count; i++)
+            {
+                if (_selectObj.name == _imageName[i])
+                {
+                    Vector3 pos = stages[i].transform.localPosition;
+                    pos.x = 0;
+                    stages[i].transform.localPosition = pos;
+                    pos.x = 1200;
+                    stages[(i + 1) % stages.Count].transform.localPosition = pos;
+                    pos.x = -1200;
+                    stages[(i + stages.Count - 1) % stages.Count].transform.localPosition = pos;
+                    _nowSelected = i;
+                }
+            }
+        }
         if (!isCalledOnce)
         {
             ///ここを任意のボタンにしましょう。
             if (Input.GetKeyDown("space"))
             {
                 isCalledOnce = true;
-                FadeManager.Instance.LoadScene("BattleScene", 2.0f);
-                Debug.Log("Batlleへ");
+                FadeManager.Instance.LoadScene("BattleScene_0" + $"{_nowSelected + 1}", 2.0f);
+                Debug.Log("BattleSceneへ");
             }
         }
     }
--- HCMs/Assets/Scenes/SelectScene/Script/TimeSel.cs
+++ HCMs/Assets/Scenes/SelectScene/Script/TimeSel.cs
@@ -1,18 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class TimeSel : MonoBehaviour
 {
+    enum Stage { TimeAttackScene };
+    GameObject _nowSelect;
+    public List<Image> stages;
+    [SerializeField] EventSystem eventSystem;
+    GameObject _selectObj;
+    List<string> _imageName;
+
     void Start()
     {
-
+        var tmp = stages.Count;
+        _imageName = new List<string>(tmp);
+        for (int i = 0; i < tmp; i++)
+        {
+            _imageName.Add($"{i}");
+        }
     }
 
     bool isCalledOnce = false;
 
+    // Update is called once per frame
     void Update()
     {
+        if (eventSystem.currentSelectedGameObject.gameObject == _selectObj)
+        {
+        }
+        else
+        {
+            _selectObj = eventSystem.currentSelectedGameObject.gameObject;
+            for (int i = 0; i < stages.Count; i++)
+            {
+                if (_selectObj.name == _imageName[i])
+                {
+                    Vector3 pos = stages[i].transform.localPosition;
+                    pos.x = 0;
+                    stages[i].transform.localPosition = pos;
+                    pos.x = 1200;
+                    stages[(i + 1) % stages.Count].transform.localPosition = pos;
+                    pos.x = -1200;
+                    stages[(i + stages.Count - 1) % stages.Count].transform.localPosition = pos;
+                }
+            }
+        }
         if (!isCalledOnce)
         {
             ///ここを任意のボタンにしましょう。
@@ -20,7 +55,7 @@ public class TimeSel : MonoBehaviour
             {
                 isCalledOnce = true;
                 FadeManager.Instance.LoadScene("TimeAttackScene", 2.0f);
-                Debug.Log("TAへ");
+                Debug.Log("01へ");
             }
         }
     }
