cd3ca939d612dfd09b531dc03a8cd6c325c3c3cf
DaichiNakagama <nakadaichi@icloud.com>
2020-06-16 10:55:52 +0900

    Resporn

---
+++ HCMs/Assets/Control/BoxCollision.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxCollision : MonoBehaviour
+{
+    GameObject refObj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        refObj = GameObject.Find("RacingCar2");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        Resporn r1 = refObj.GetComponent<Resporn>();
+        r1.SetResBox(this.transform);
+        Debug.Log("すり抜けた！");
+    }
+}
