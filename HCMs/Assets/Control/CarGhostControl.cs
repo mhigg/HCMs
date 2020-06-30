6319e7938a2c88179310dab5fb38aad3f6c08f88
daiiiiiiiiiiiii <nakadaichi@icloud.com>
2020-06-30 15:41:24 +0900

    不足分

---
+++ HCMs/Assets/Control/CarGhostControl.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+namespace UnityStandardAssets.Vehicles.Car
+{
+    [RequireComponent(typeof(CarController))]
+    public class CarGhostControl : MonoBehaviour
+    {
+        private CarController m_Car; // 参照するスクリプト
+
+        private void Awake()
+        {
+            m_Car = GetComponent<CarController>();
+        }
+
+
+        private void FixedUpdate()
+        {
+            // データを読み込んで当てはめる
+            float h = CrossPlatformInputManager.GetAxis("Horizontal");
+            float v = CrossPlatformInputManager.GetAxis("Vertical");
+#if !MOBILE_INPUT
+            float handbrake = CrossPlatformInputManager.GetAxis("Jump");
+            m_Car.Move(h, v, v, handbrake);
+#else
+            m_Car.Move(h, v, v, 0f);
+#endif
+        }
+    }
+}
+
