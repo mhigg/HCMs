a2400e365b108e6bb7c46d71c4e2c65fb5576150
makou <tekito.email>
2020-06-30 12:37:41 +0900

    バトルシーン01の車でバトルシーン02のリスポーン機能を試す リスポーン機能の処理の修正

--- HCMs/Assets/Control/BoxCollision.cs
+++ HCMs/Assets/Control/BoxCollision.cs
@@ -4,23 +4,24 @@ using UnityEngine;
 
 public class BoxCollision : MonoBehaviour
 {
-    GameObject refObj;
     // Start is called before the first frame update
     void Start()
     {
-        refObj = GameObject.Find("RacingCar2");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 
     void OnTriggerEnter()
     {
-        Resporn r1 = refObj.GetComponent<Resporn>();
-        r1.SetResBox(this.transform);
-        Debug.Log("すり抜けた！");
+        GameObject[] refObj = GameObject.FindGameObjectsWithTag("RacingCar");
+        foreach (GameObject car in refObj)
+        {
+            Resporn resporn = car.gameObject.GetComponentInParent<Resporn>();
+            resporn.SetResBox(this.transform);
+            Debug.Log("すり抜けた！" + car.name);
+        }
     }
 }
