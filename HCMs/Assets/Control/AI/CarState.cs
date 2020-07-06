ab9bbe3b7f3936e344c49e5af444c1ef47154fa3
daiiiiiiiiiiiii <nakadaichi@icloud.com>
2020-07-06 13:45:49 +0900

    スクリプト追加

---
+++ HCMs/Assets/Control/AI/CarState.cs
@@ -0,0 +1,15 @@
+﻿// 車の状態によって行う操作を定義するクラス
+public interface CarState
+{
+    CarState Move();        // 移動//
+    CarState Straight();    // //直進
+    CarState Brake();       // //止まる
+    CarState Curve();       // //曲がる
+    CarState Back();        // //バック
+    CarState Dodge();       // よける
+    CarState Hinder();      // 邪魔する/*
+    CarState FrontHider();  // /*前に出て邪魔する 
+    CarState SideHider();   // /*横から邪魔する
+    CarState BackHider();   // /*後ろから邪魔する
+    CarState 
+}
