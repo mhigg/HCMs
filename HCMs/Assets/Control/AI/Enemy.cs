9e47b90c247af402a6fe4046b2c15fd46c290c68
DaichiNakagama <nakadaichi@icloud.com>
2020-07-07 13:02:14 +0900

    tuika

---
+++ HCMs/Assets/Control/AI/Enemy.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    //追従
+    void Following()
+    {
+
+    }
+}
