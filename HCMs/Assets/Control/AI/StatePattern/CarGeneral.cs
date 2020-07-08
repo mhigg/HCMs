b8b41b23a55f1675be7e8e21cf582487a82d802c
daiiiiiiiiiiiii <nakadaichi@icloud.com>
2020-07-08 13:18:20 +0900

    スクリプト　訂正

---
+++ HCMs/Assets/Control/AI/StatePattern/CarGeneral.cs
@@ -0,0 +1,13 @@
+﻿// CPUの情報を統括するクラス
+public class CarGeneral
+{
+    // 車の状態
+    private CarState _state = null;
+    public CarGeneral()
+    {
+        if(_state == null)
+        {
+            _state = null; 
+        }
+    }
+}
---
+++ HCMs/Assets/Control/AI/StatePattern/CarState.cs
@@ -0,0 +1,6 @@
+﻿public interface CarState
+{
+    CarState HitrayCast();      // レイが道なりにある時
+    CarState ExitRayCast();     // レイが道から外れた時   
+    CarState SerchEnemy();      // 敵がレイにかかった時
+}
