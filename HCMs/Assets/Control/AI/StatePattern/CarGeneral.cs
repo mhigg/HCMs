deb03742bab47de67429ad331d4835ea6f0d1b6f
daiiiiiiiiiiiii <nakadaichi@icloud.com>
2020-07-14 15:25:44 +0900

    すくリプ世の変更

--- HCMs/Assets/Control/AI/StatePattern/CarGeneral.cs
+++ HCMs/Assets/Control/AI/StatePattern/CarGeneral.cs
@@ -1,17 +1,24 @@
 ﻿// CPUの情報を統括するクラス
-using System;
+using System.Collections;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
+
 namespace UnityStandardAssets.Vehicles.Car
 {
     [RequireComponent(typeof(CarController))]
     public class CarGeneral : MonoBehaviour
     {
-        private CarState _state = null; // 車の状態
-        private CarController _carCtl;  // 車の操作
+        private RaycastHit[] _hit = new RaycastHit[2];
+        private CarState _state = null;     // 車の状態
+        private CarController _carCtl;      // 車の操作
+        private Vector3 _offset = new Vector3(0,0.2f,-4.5f);
+        float _dis = 10.0f;
 
-
-
+        Vector3[] _dir = new Vector3[]      // レイの方向
+        { 
+            new Vector3(-1f,-0.25f,-3f),        // 右
+            new Vector3(1f,-0.25f,-3f)          // 左
+        };
 
         private void Awake()
         {
@@ -24,17 +31,27 @@ namespace UnityStandardAssets.Vehicles.Car
             {
                 _state = new IdleState();
             }
-            
         }
 
-        public void Update()
+        void FixedUpdate()
         {
-            // レイキャスト
+            for (int i = 0; i < _dir.Length; i++)
+            {
+                var pos = transform.TransformPoint(_offset);
+                var dir = transform.TransformDirection(_dir[i]);
+                // レイキャストで判定
+                // 判定あればHitRayCast
+                if(Physics.Raycast(pos, dir, out _hit[i], _dis))
+                {
+                    this._state.HitRaycastWay();
+                }
 
-            // 判定あればHitRayCast
+
+                DebugDraw(pos, dir, _dis, _hit[i].collider);
+            }
 
             float h = CrossPlatformInputManager.GetAxis("Horizontal");
-            float v = -0.5f;
+            float v = CrossPlatformInputManager.GetAxis("Vertical");
             float handbrake = CrossPlatformInputManager.GetAxis("Jump");
             _carCtl.Move(h, v, v, handbrake);
         }
@@ -55,6 +72,12 @@ namespace UnityStandardAssets.Vehicles.Car
         public CarState SerchEnemyEvent()
         {
             return _state.SerchEnemy();
+        }
+
+        void DebugDraw(Vector3 vec1,Vector3 vec2,float f,Collider col)
+        {
+            Color color = col ? Color.red : Color.green;
+            Debug.DrawRay(vec1, vec2.normalized * f, color, 0,false);
         }
     }
 }
