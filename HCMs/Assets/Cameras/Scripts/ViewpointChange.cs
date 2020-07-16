a62356aa0bb368ceebd61ba46c29cfad4f0b7e6f
hidaka <shadow.konbu@icloud.com>
2020-07-16 13:36:30 +0900

    視点変更カメラ追加

---
+++ HCMs/Assets/Cameras/Scripts/ViewpointChange.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViewpointChange : MonoBehaviour
+{
+    [SerializeField] private GameObject _pivotCamera;
+    [SerializeField] private GameObject _insideCamera;
+    [SerializeField] private GameObject _frontCamera;
+    [SerializeField] private GameObject _upsideCamera;
+    private int _keyCount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _pivotCamera.SetActive(true);
+        _insideCamera.SetActive(false);
+        _frontCamera.SetActive(false);
+        _upsideCamera.SetActive(false);
+
+        _keyCount = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ++_keyCount;
+        }
+        if (_keyCount >= 4) _keyCount = 0;
+
+        switch (_keyCount)
+        {
+            case 0:
+                _pivotCamera.SetActive(true);
+                _insideCamera.SetActive(false);
+                _frontCamera.SetActive(false);
+                _upsideCamera.SetActive(false);
+                break;
+            case 1:
+                _pivotCamera.SetActive(false);
+                _insideCamera.SetActive(true);
+                break;
+            case 2:
+                _insideCamera.SetActive(false);
+                _frontCamera.SetActive(true);
+                break;
+            case 3:
+                _frontCamera.SetActive(false);
+                _upsideCamera.SetActive(true);
+                break;
+            default:
+                break;
+        }
+
+    }
+}
