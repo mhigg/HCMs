d88d54cb8b4c88f0d9e4f7cf24a61f2e3507eef1
mhigg <tekito@mail>
2020-08-07 13:44:32 +0900

    数字→数字画像変換スクリプト追加 数字画像をスプライト化 タイムアタックで使用中のキャンバスをタイムアタック用キャンバスとしてプレハブ化

---
+++ HCMs/Assets/UI/TransImgNo/ImageNo.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageNo : SpriteNo<Image>
+{
+    [SerializeField]
+    private bool _raycastTarget = false;
+
+    // 新しく作ったImageの初期化
+    protected override void InitComponent(Image image)
+    {
+        image.raycastTarget = _raycastTarget;
+    }
+
+    // Spriteを更新
+    protected override void UpdateComponent(Image image, Sprite sprite, Color color)
+    {
+        image.sprite = sprite;
+        image.color = color;
+        image.SetNativeSize();
+    }
+
+    // RaycastTargetの設定を変更する
+    public void ChangeRaycastTarget(bool raycastTarget)
+    {
+        _raycastTarget = raycastTarget;
+        InitComponents();
+    }
+
+}
---
+++ HCMs/Assets/UI/TransImgNo/SpriteRendererNo.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class SpriteRendererNo : SpriteNo<SpriteRenderer>
+{
+
+    [SerializeField]
+    private string _sortingLayerName = "Default";
+
+    [SerializeField]
+    private int _sortingOrder = 0;
+
+    // 新しく作ったSpriteRendererの初期化
+    protected override void InitComponent(SpriteRenderer spriteRenderer)
+    {
+        spriteRenderer.sortingLayerName = _sortingLayerName;
+        spriteRenderer.sortingOrder = _sortingOrder;
+    }
+
+    // Spriteを更新
+    protected override void UpdateComponent(SpriteRenderer spriteRenderer, Sprite sprite, Color color)
+    {
+        spriteRenderer.sprite = sprite;
+        spriteRenderer.color = color;
+    }
+
+    // sortingLayerNameの設定を変更する
+    public void ChangeSortingLayerName(string sortingLayerName)
+    {
+        _sortingLayerName = sortingLayerName;
+        InitComponents();
+    }
+
+    // sortingOrderの設定を変更する
+    public void ChangeSortingOrder(int sortingOrder)
+    {
+        _sortingOrder = sortingOrder;
+        InitComponents();
+    }
+
+}
