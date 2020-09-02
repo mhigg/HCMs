98bd9b710570e30d1fd8da1f9bb5693b695afdda
hidaka <shadow.konbu2018@outlook.jp>
2020-09-03 00:18:40 +0900

    RacingCar_2.prefabと車体選択シーンの更新

--- HCMs/Assets/UI/Script/CarCustom.cs
+++ HCMs/Assets/UI/Script/CarCustom.cs
@@ -7,15 +7,19 @@ public class CarCustom : MonoBehaviour
     [SerializeField] private GameObject _carObj;
     Vector3 _roteVec;
 
-    // Start is called before the first frame update
     void Start()
     {
         _roteVec = new Vector3(0f, 1.5f, 0f);
     }
 
-    // Update is called once per frame
     void Update()
     {
         _carObj.gameObject.transform.Rotate(_roteVec);
+        ModelSelecting();
+    }
+
+    void ModelSelecting()
+    {
+        DontDestroyOnLoad(_carObj);
     }
 }
