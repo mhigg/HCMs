f910dd7596e0a47624e72005aaf16edcaa5d9696
hidaka <shadow.konbu2018@outlook.jp>
2020-09-07 18:43:41 +0900

    custom関係

--- HCMs/Assets/UI/Image/OutputCarSpeed.cs
+++ HCMs/Assets/UI/Image/OutputCarSpeed.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 using UnityStandardAssets.Vehicles.Car;
 
 public class OutputCarSpeed : MonoBehaviour
@@ -14,18 +15,32 @@ public class OutputCarSpeed : MonoBehaviour
     private Image speedImage = null;
     [SerializeField]
     private float percentage = 0f;
+    private bool firstRun;
 
     // Start is called before the first frame update
     void Start()
     {
         carController = GetComponent<CarController>();
 
-        imageNo = imageNo.GetComponent<ImageNo>();
+        firstRun = true;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (SceneManager.GetActiveScene().name != "BattleCustom"
+        && SceneManager.GetActiveScene().name != "TimeAttackCustom")
+        {
+            if (firstRun)
+            {
+                GameObject speedImgNo = GameObject.Find("SpeedImageNo");
+                imageNo = speedImgNo.GetComponent<ImageNo>();
+
+                GameObject speedImg = GameObject.Find("SpeedMeterImage");
+                speedImage = speedImg.GetComponent<Image>();
+            }
+        }
+
         //　Imageの表示率を最大速度に対する現在の速度で計算する
         var ratio = Mathf.InverseLerp(0f, 1f, Mathf.Abs(carController.CurrentSpeed) / 140f);
         //　速度用のImageの最小と最大を補正した値で計算
