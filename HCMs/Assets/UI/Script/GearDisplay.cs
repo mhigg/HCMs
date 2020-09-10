4965039e02ab1d3c74bdb5f54b20425e92d3baa4
makou <tekito.email>
2020-09-10 17:04:33 +0900

    ステージ２、４、５のBGM設定 相手がCPUの時に画面分割をしないように変更（２，４，５のみ） ギアが2Pの数値しか反映されない不具合の修正

--- HCMs/Assets/UI/Script/GearDisplay.cs
+++ HCMs/Assets/UI/Script/GearDisplay.cs
@@ -7,21 +7,17 @@ using UnityStandardAssets.Vehicles.Car;
 public class GearDisplay : MonoBehaviour
 {
     public ImageNo imageNo = null;
+    public int playerNo;
 
     [SerializeField] private Text m_GearTex = null;
     [SerializeField] private CarController m_Car = null;
-    private int m_GearNum;
+//    private int m_GearNum;
     private GameObject carObj;
-    private int carNumMax;
 
     // Start is called before the first frame update
     void Start()
     {
-        carNumMax = FindInfoByScene.Instance.GetPlayerNum();
-        for(int i = 0; i < carNumMax; i++)
-        {
-            carObj = GameObject.FindGameObjectsWithTag("RacingCar")[i].transform.parent.gameObject;
-        }
+        carObj = GameObject.FindGameObjectsWithTag("RacingCar")[playerNo].transform.parent.gameObject;
 
         m_Car = carObj.GetComponent<CarController>();
         m_GearTex = m_GearTex.GetComponent<Text>();
@@ -32,7 +28,7 @@ public class GearDisplay : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        m_GearNum = m_Car.CurrentGearNum;
+//        m_GearNum = m_Car.CurrentGearNum;
 //        m_GearTex.text = $"{m_Car.CurrentGearNum + 1}";
         imageNo.SetNo(m_Car.CurrentGearNum + 1);
     }
