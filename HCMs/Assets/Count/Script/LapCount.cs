1a9db73519826d1643b28894b7b5d50eb9c20be7
makou <tekito.email>
2020-09-10 18:50:21 +0900

    バトルの周回数表示を変更

--- HCMs/Assets/Count/Script/LapCount.cs
+++ HCMs/Assets/Count/Script/LapCount.cs
@@ -7,7 +7,7 @@ using UnityEngine.SceneManagement;
 // 周回数をプレイヤーごとにカウントする
 public class LapCount : MonoBehaviour
 {
-    public TextMeshProUGUI lapText;     // 周回数表示
+    //public TextMeshProUGUI lapText;     // 周回数表示
 
     private int _lapCnt;        // ラップカウント
     private int _lapMax;        // 最大周回数
@@ -18,18 +18,18 @@ public class LapCount : MonoBehaviour
         _lapCnt = 1;    // 初期値は１
         _lapMax = FindInfoByScene.Instance.GetLapMax(SceneManager.GetActiveScene().name);
 
-        lapText = lapText.GetComponent<TextMeshProUGUI>();
-        lapText.text = _lapCnt + " / " + _lapMax;
+        //lapText = lapText.GetComponent<TextMeshProUGUI>();
+        //lapText.text = _lapCnt + " / " + _lapMax;
     }
 
     public void CountLap()
     {
         _lapCnt++;
-        if (_lapCnt <= _lapMax)
-        {
-            lapText.text = _lapCnt + " / " + _lapMax;
-        }
-        Debug.Log(lapText.text);
+        //if (_lapCnt <= _lapMax)
+        //{
+        //    lapText.text = _lapCnt + " / " + _lapMax;
+        //}
+        //Debug.Log(lapText.text);
     }
 
     public bool CheckLapCount()
