7f1660f7cf43b14c6ace5f8bcdd6a05f93139c25
mhigg <tekito@mail>
2020-09-14 00:20:34 +0900

    ＦＩＮＩＳＨ表示の位置調整

--- HCMs/Assets/Control/BattleModeCamera.cs
+++ HCMs/Assets/Control/BattleModeCamera.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 public class BattleModeCamera : MonoBehaviour
 {
+    private bool _isAllScreen;  // カメラが全画面かどうか
+
     void Start()
     {
         if(GameObject.FindGameObjectsWithTag("CPU").Length > 0)
@@ -10,12 +12,19 @@ public class BattleModeCamera : MonoBehaviour
             // CPUがいるなら画面分割なしのカメラに切り替える
             this.transform.Find("PerfectCamera01").gameObject.SetActive(false);
             this.transform.Find("PerfectCamera11").gameObject.SetActive(true);
+            _isAllScreen = true;
         }
         else
         {
             // CPUがいない(＝2Pがいる)なら画面分割ありのカメラに切り替える
             this.transform.Find("PerfectCamera01").gameObject.SetActive(true);
             this.transform.Find("PerfectCamera11").gameObject.SetActive(false);
+            _isAllScreen = false;
         }
+    }
+
+    public bool IsAllScreen()
+    {
+        return _isAllScreen;
     }
 }
--- HCMs/Assets/StageModel/GoalObject/Script/GoalFlag.cs
+++ HCMs/Assets/StageModel/GoalObject/Script/GoalFlag.cs
@@ -77,7 +77,15 @@ public class GoalFlag : MonoBehaviour
                 if (throughObject.GetComponent<LapCount>().CheckLapCount())
                 {
                     Debug.Log("プレイヤー" + playerID + "ゴール");
-                    Vector3 pos = new Vector3((_playerNum - 1) * 500 * (playerID * 2 - 1) + 960, 540, 0);
+                    Vector3 pos;
+                    if(throughObject.transform.parent.GetComponent<BattleModeCamera>().IsAllScreen())
+                    {
+                        pos = new Vector3(Screen.width / 2, Screen.height / 2, 0);
+                    }
+                    else
+                    {
+                        pos = new Vector3((_playerNum - 1) * 500 * (playerID * 2 - 1) + Screen.width / 2, Screen.height / 2, 0);
+                    }
                     Image _finishImage = Instantiate(finishImage, pos, Quaternion.identity);
                     _finishImage.transform.SetParent(GameObject.FindGameObjectWithTag("CounterCanvas").transform);
                     timeCounter.FinishCount(playerID);
